874b4c3cb5cee4a50b38019edc9286193d43c558
Robbie Ferguson <contact@category5.tv>
2017-04-24 10:44:36 -0400

    Update info.sh

--- info.sh
+++ info.sh
@@ -4,13 +4,13 @@
 export COMMAND=$1
 me=`basename "$0"`
 USAGE="Usage: ./$me COMMAND"
-if [ $COMMAND -eq "ip" ]; then
+if [ $COMMAND == "ip" ]; then
   /sbin/ip -f inet addr show eth0 | grep -Po 'inet \K[\d.]+'
-elif [ $COMMAND -eq "nemsver" ]; then
+elif [ $COMMAND == "nemsver" ]; then
   /bin/cat /var/www/html/inc/ver.txt
-elif [ $COMMAND -eq "nemsveravail" ]; then
+elif [ $COMMAND == "nemsveravail" ]; then
   /bin/cat /var/www/html/inc/ver-available.txt
-elif [ $COMMAND -eq "users" ]; then
+elif [ $COMMAND == "users" ]; then
   export USERCOUNT=`/usr/bin/users | /usr/bin/wc -w`
   if [ $USERCOUNT -eq 1 -o $USERCOUNT -eq -1 ]
 		then
