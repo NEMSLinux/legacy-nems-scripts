c6ae0beb2437bac8c574098b61dd88050f0b2f48
Robbie Ferguson <contact@category5.tv>
2017-04-24 10:39:00 -0400

    Update info.sh

--- info.sh
+++ info.sh
@@ -10,6 +10,14 @@ elif [ $COMMAND = "nemsver" ]; then
   /bin/cat /var/www/html/inc/ver.txt
 elif [ $COMMAND = "nemsveravail" ]; then
   /bin/cat /var/www/html/inc/ver-available.txt
+elif [ $COMMAND = "users" ]; then
+  export USERCOUNT=`/usr/bin/users | /usr/bin/wc -w`
+  if [ $USERCOUNT -eq 1 -o $USERCOUNT -eq -1 ]
+		then
+			echo $USERCOUNT user
+		else
+			echo $USERCOUNT users
+	fi
 else
   echo $USAGE
 fi
