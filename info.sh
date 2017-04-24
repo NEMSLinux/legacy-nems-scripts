1e11eb1b695d612d73835104f3473d687b6ab993
Robbie Ferguson <contact@category5.tv>
2017-04-24 10:30:05 -0400

    Update info.sh

--- info.sh
+++ info.sh
@@ -6,6 +6,10 @@ me=`basename "$0"`
 USAGE="Usage: ./$me COMMAND"
 if [ $COMMAND = "ip" ]; then
   /sbin/ip -f inet addr show eth0 | grep -Po 'inet \K[\d.]+'
+else if [ $COMMAND = "nemsver"]; then
+  /bin/cat /var/www/html/inc/ver.txt
+else if [ $COMMAND = "nemsveravail"]; then
+  /bin/cat /var/www/html/inc/ver-available.txt
 else
   echo $USAGE
 fi
