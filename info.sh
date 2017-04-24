910f8f4b748f520462f4a56f46ee0242aeb42217
root <root@raspberrypi>
2017-04-24 14:26:39 +0000

    IP

--- info.sh
+++ info.sh
@@ -4,8 +4,8 @@
 export COMMAND=$1
 me=`basename "$0"`
 USAGE="Usage: ./$me COMMAND"
-if [ -z "$COMMAND" ]; then echo $USAGE; exit; fi
-
-if [ -z "$COMMAND" == "ip" ]; then
+if [ $COMMAND = "ip" ]; then
   /sbin/ip -f inet addr show eth0 | grep -Po 'inet \K[\d.]+'
-exit; fi
+else
+  echo $USAGE
+fi
