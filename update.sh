a7629101ea539b914b6dd0a80b84207070eae733
Robbie Ferguson <contact@category5.tv>
2017-04-24 11:04:24 -0400

    Force root user

--- update.sh
+++ update.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# Wait 30 seconds to prevent issues at boot with a missing version file during transit
-sleep 30
+if [[ $EUID -ne 0 ]]; then
+  echo "ERROR: This script must be run as root" 2>&1
+  exit 1
+else
+  # Wait 30 seconds to prevent issues at boot with a missing version file during transit
+  sleep 30
 
-# Tell the web cache to serve up the file from midnight
-timestamp=$( /bin/date --date="today 00:00:01 UTC -5 hours" +%s )
-/usr/bin/wget -q -O /var/www/html/inc/ver-available.txt http://cdn.zecheriah.com/baldnerd/nems/ver-current.txt#$timestamp
+  # Tell the web cache to serve up the file from midnight
+  timestamp=$( /bin/date --date="today 00:00:01 UTC -5 hours" +%s )
+  /usr/bin/wget -q -O /var/www/html/inc/ver-available.txt http://cdn.zecheriah.com/baldnerd/nems/ver-current.txt#$timestamp
 
-# Update nems-migrator
-cd /root/nems/nems-migrator && git pull
+  # Update nems-migrator
+  cd /root/nems/nems-migrator && git pull
 
-# Copy the version data to the public inc folder (in case it accidentally gets deleted)
-test -d "/var/www/html/inc" || mkdir -p "/var/www/html/inc" && cp /root/nems/ver.txt "/var/www/html/inc"
+  # Copy the version data to the public inc folder (in case it accidentally gets deleted)
+  test -d "/var/www/html/inc" || mkdir -p "/var/www/html/inc" && cp /root/nems/ver.txt "/var/www/html/inc"
+fi
