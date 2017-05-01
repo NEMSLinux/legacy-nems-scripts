f8614f38c235ef4aec84291ee2870dba407b3718
Robbie Ferguson <contact@category5.tv>
2017-05-01 12:09:02 -0400

    Remove manual rpimonitor update
    Switched to RPi-Monitor automatic update for packages service.

--- update.sh
+++ update.sh
@@ -10,9 +10,6 @@ else
   # Tell the web cache to serve up the file from midnight
   timestamp=$( /bin/date --date="today 00:00:01 UTC -5 hours" +%s )
   /usr/bin/wget -q -O /var/www/html/inc/ver-available.txt http://cdn.zecheriah.com/baldnerd/nems/ver-current.txt#$timestamp
-
-  # Update RPi-Monitor Package Status
-  /etc/init.d/rpimonitor update
   
   # Update nems-migrator
   cd /root/nems/nems-migrator && git pull
