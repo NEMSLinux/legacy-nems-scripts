5e28385984129acd17c8d03273b0b70191fb799d
Robbie Ferguson <contact@category5.tv>
2017-05-05 16:10:53 -0400

    Add comment

--- monitorix.sh
+++ monitorix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script generates all the images for Monitorix via cronjob rather than on-demand, giving the perception of much faster processing and reducing load on page refresh
+# It also ensures certain graphs (ie. Yearly) are not updated every time since the data won't be affected
+
 /bin/sleep 15
 
 if [[ $1 = "all" ]] || [[ $1 = "day" ]]; then /usr/bin/nice -n19 /usr/bin/w3m -dump "http://localhost:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1day&color=black" > /dev/null 2>&1; fi;
