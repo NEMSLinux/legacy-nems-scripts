e568b6318b94097af0993316388a9b57f3c43fb9
Cat5TV <contact@category5.tv>
2017-05-30 01:15:35 +0000

    Wait for network, retry if fail

--- monitorix.sh
+++ monitorix.sh
@@ -2,11 +2,21 @@
 # This script generates all the images for Monitorix via cronjob rather than on-demand, giving the perception of much faster processing and reducing load on page refresh
 # It also ensures certain graphs (ie. Yearly) are not updated every time since the data won't be affected
 
+  # Ping Google to see if Internet is up. Don't begin until we have Internet.
+  count=1
+  while ! ping -c 1 -W 1 google.com; do
+    if [ $count -eq 60 ]
+      then
+         echo "Google not responding. Resuming, but if Internet is down, updates will fail."
+         break;
+    fi     
+    ((count++))
+    sleep 1
+  done
+
 # Sometimes Monitorix (the daemon) stops responding even though it is running.
 # Restart it here just in case, before loading the data.
 /bin/systemctl restart monitorix
-
-/bin/sleep 15
 
 # Detect the default network interface and use it for net graphs
 adapter=`/sbin/route | /bin/grep '^default' | /bin/grep -o '[^ ]*$'`
@@ -29,10 +39,29 @@ fi
   fi;
 rm /tmp/monitorix.nems
 
-/bin/sleep 5
-
 # Generate the graphs
-if [[ $1 = "all" ]] || [[ $1 = "day" ]]; then /usr/bin/nice -n19 /usr/bin/w3m -dump "http://localhost:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1day&color=black" > /dev/null 2>&1; fi;
-if [[ $1 = "all" ]] || [[ $1 = "week" ]]; then /usr/bin/nice -n19 /usr/bin/w3m -dump "http://localhost:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1week&color=black" > /dev/null 2>&1; fi;
-if [[ $1 = "all" ]] || [[ $1 = "month" ]]; then /usr/bin/nice -n19 /usr/bin/w3m -dump "http://localhost:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1month&color=black" > /dev/null 2>&1; fi;
-if [[ $1 = "all" ]] || [[ $1 = "year" ]]; then /usr/bin/nice -n19 /usr/bin/w3m -dump "http://localhost:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1year&color=black" > /dev/null 2>&1; fi;
+
+if [[ $1 = "all" ]] || [[ $1 = "day" ]]; then
+  until $(/usr/bin/curl --output /dev/null --silent --fail "http://127.0.0.1:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1day&color=black"); do
+    sleep .5
+  done;
+fi;
+
+if [[ $1 = "all" ]] || [[ $1 = "week" ]]; then
+  until $(/usr/bin/curl --output /dev/null --silent --fail "http://127.0.0.1:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1week&color=black"); do
+    sleep .5
+  done;
+fi;
+
+if [[ $1 = "all" ]] || [[ $1 = "month" ]]; then
+  until $(/usr/bin/curl --output /dev/null --silent --fail "http://127.0.0.1:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1month&color=black"); do
+    sleep .5
+  done;
+fi;
+
+if [[ $1 = "all" ]] || [[ $1 = "year" ]]; then
+  until $(/usr/bin/curl --output /dev/null --silent --fail "http://127.0.0.1:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1year&color=black"); do
+    sleep .5
+  done;
+fi;
+
