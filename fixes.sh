493519676236aafffa87536cc9163973503be6a1
Robbie Ferguson <contact@category5.tv>
2017-06-06 18:14:09 -0400

    Patch cgi.cfg
    Fix admin access for certain functions within Nagios Core for nems-init created user. This is the retroactive version which fixes NEMS Server deployments from before nems-init was patched.

--- fixes.sh
+++ fixes.sh
@@ -2,3 +2,12 @@
 
 # NEMS 1.2.1 was released with an incorrect permission on this file
 chown www-data:www-data /etc/nagios3/global/timeperiods.cfg
+
+# Fix Nagios Core access to admin features for user created with nems-init
+if [ -f /var/www/htpasswd ]; then
+  if grep -q nagiosadmin /etc/nagios3/cgi.cfg; then
+    username=$(cut -f1 -d":" /var/www/htpasswd)
+    cp -f /root/nems/nems-migrator/data/nagios/cgi.cfg /etc/nagios3/
+    /bin/sed -i -- 's/nagiosadmin/'"$username"'/g' /etc/nagios3/cgi.cfg
+  fi
+fi
