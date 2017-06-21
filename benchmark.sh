e44ba079aa13c82b6ec57107aad36bd5be3a8ff8
Robbie Ferguson <contact@category5.tv>
2017-06-21 10:38:34 -0400

    Create benchmark.sh

---
+++ benchmark.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "NEMS System Benchmark... Please Wait (may take a while)."
+
+echo "NEMS System Benchmark" > /tmp/nems-benchmark.log
+date >> /tmp/nems-benchmark.log
+printf "NEMS Version: " >> /tmp/nems-benchmark.log
+/home/pi/nems-scripts/info.sh nemsver >> /tmp/nems-benchmark.log
+printf "LAN IP: " >> /tmp/nems-benchmark.log
+/home/pi/nems-scripts/info.sh ip >> /tmp/nems-benchmark.log
+
+echo "---------------------------------" >> /tmp/nems-benchmark.log
+
+echo "SD Card READ:" >> /tmp/nems-benchmark.log
+/sbin/hdparm -t /dev/mmcblk0p2 >> /tmp/nems-benchmark.log
+echo "SD Card WRITE:" >> /tmp/nems-benchmark.log
+/bin/dd count=1k bs=1M if=/dev/zero of=/root/nems-benchmark.img >> /tmp/nems-benchmark.log
+rm /root/nems-benchmark.img
+
+echo "---------------------------------" >> /tmp/nems-benchmark.log
+
+#echo "Memory READ:" >> /tmp/nems-benchmark.log
+echo "Memory WRITE:" >> /tmp/nems-benchmark.log
+/bin/dd count=1k bs=1M if=/dev/zero of=/tmp/nems-benchmark.img >> /tmp/nems-benchmark.log
+rm /tmp/nems-benchmark.img
+
+echo "---------------------------------" >> /tmp/nems-benchmark.log
+
+echo "Filesystem:" >> /tmp/nems-benchmark.log
+/bin/df -h >> /tmp/nems-benchmark.log
+
+echo "---------------------------------" >> /tmp/nems-benchmark.log
+
+echo "Memory:" >> /tmp/nems-benchmark.log
+/usr/bin/free -h >> /tmp/nems-benchmark.log
+
+cat /tmp/nems-benchmark.log
+rm  /tmp/nems-benchmark.log
