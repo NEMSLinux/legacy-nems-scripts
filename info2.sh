982007ee97c35c70be7dd935ae8bee56fe3e7bef
Cat5TV <contact@category5.tv>
2017-10-06 16:03:02 -0400

    Yay

---
+++ info2.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/php
+<?php
+ /*
+  This is where all the PHP scripts are for nems-info.
+  These are called with the nems-info command, not direct.
+ */
+
+if (!isset($argv[1])) exit('Invalid usage. Please use the nems-info command.' . PHP_EOL);
+
+switch($argv[1]) {
+  case 1: // temperature
+    $monitorix = monitorix('raspberrypi');
+    if (is_array($monitorix['data']['rpi_temp0'])) {
+      $tmp = 0;
+      $count = 0;
+      foreach ($monitorix['data']['rpi_temp0'] as $date=>$temperature) {
+        $tmp = ($tmp + $temperature);
+        $count++;
+      }
+      $average_temperature = ($tmp/$count);
+      echo $average_temperature . PHP_EOL;
+    }
+  break;
+}
+
+
+
+
+function monitorix($db) {
+
+  switch ($db) {
+
+    case 'apache':
+      return rrd_fetch( "/var/lib/monitorix/apache.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+    case 'fs':
+      return rrd_fetch( "/var/lib/monitorix/fs.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+    case 'int':
+      return rrd_fetch( "/var/lib/monitorix/int.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+    case 'kern':
+      return rrd_fetch( "/var/lib/monitorix/kern.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+    case 'net':
+      return rrd_fetch( "/var/lib/monitorix/net.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+    case 'raspberrypi':
+      return rrd_fetch( "/var/lib/monitorix/raspberrypi.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+    case 'system':
+      return rrd_fetch( "/var/lib/monitorix/system.rrd", array( "AVERAGE", "--resolution", "60", "--start", "-1d", "--end", "start+1h" ) );
+    break;
+
+  }
+
+}
+
+
+?>
