ccee58df0b14991872d8a127d9e1991d4cc58be2
Cat5TV <contact@category5.tv>
2017-11-13 09:20:49 -0500

    Test using snakeoil certs

---
+++ testfix.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo Testing fix of SSL...
+
+printf "Backing up your current settings... "
+if [[ ! -f /etc/apache2/sites-available/000-default.conf~ ]]; then
+  cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf~
+  echo Done.
+else
+  echo Not needed - was already backed up.
+fi
+
+printf "Generating new cert... "
+/usr/sbin/make-ssl-cert generate-default-snakeoil --force-overwrite
+echo Done.
+
+printf "Patching your Apache2 Configuration... "
+  # Comment out the CA
+  if grep -q "  SSLCertificateChainFile /var/www/certs/ca.pem" /etc/apache2/sites-available/000-default.conf; then
+    /bin/sed -i -- 's,SSLCertificateChainFile,# SSLCertificateChainFile,g' /etc/apache2/sites-available/000-default.conf
+  fi
+
+  # Change the cert files
+  if grep -q "/var/www/certs/server-cert.pem" /etc/apache2/sites-available/000-default.conf; then
+    /bin/sed -i -- 's,/var/www/certs/server-cert.pem,/etc/ssl/certs/ssl-cert-snakeoil.pem,g' /etc/apache2/sites-available/000-default.conf
+  fi
+  if grep -q "/var/www/certs/server-key.pem" /etc/apache2/sites-available/000-default.conf; then
+    /bin/sed -i -- 's,/var/www/certs/server-key.pem,/etc/ssl/private/ssl-cert-snakeoil.key,g' /etc/apache2/sites-available/000-default.conf
+  fi
+echo Done.
+
+printf "Restarting Apache2..."
+/bin/systemctl restart apache2
+echo Done.
+
+echo ""
+echo Patch complete. Please test a secure connection to your NEMS server in your browser.
+echo ""
