8413ec6968fbbb32fea9b7c60c796bcd2c9c47f1
Cat5TV <contact@category5.tv>
2018-01-22 09:12:46 -0500

    Fix log

--- mailtest.sh
+++ mailtest.sh
@@ -67,7 +67,8 @@ $HOSTADDRESS = shell_exec('/usr/local/bin/nems-info ip');
 $HOSTNAME = shell_exec('hostname');
 $LONGDATETIME = date('r');
 if ($USER5 == $CONTACTEMAIL) exit('You need to send to a different email address: same as sender.' . PHP_EOL);
-$command = "/usr/bin/printf \"%b\" \"***** NEMS Test Email *****\n\nNotification Type: Test\nHost: $HOSTNAME\nAddress: $HOSTADDRESS\n\nDate/Time: $LONGDATETIME\n\" | /usr/bin/sendemail -v -s $USER7 -xu $USER9 -xp $USER10 -t $CONTACTEMAIL -f $USER5 -l /var/log/sendmail -u \"** NEMS Test Email: $HOSTNAME **\" -m \"***** NEMS Test Email *****\n\nNotification Type: Test\nHost: $HOSTNAME\nAddress: $HOSTADDRESS\n\nDate/Time: $LONGDATETIME\n\"";
+$command = "/usr/bin/printf \"%b\" \"***** NEMS Test Email *****\n\nNotification Type: Test\nHost: $HOSTNAME\nAddress: $HOSTADDRESS\n\nDate/Time: $LONGDATETIME\n\" | /usr/bin/sendemail -v -s $USER7 -xu $USER9 -xp $USER10 -t $CONTACTEMAIL -f $USER5 -l /var/log/sendemail -u \"** NEMS Test Email: $HOSTNAME **\" -m \"***** NEMS Test Email *****\n\nNotification Type: Test\nHost: $HOSTNAME\nAddress: $HOSTADDRESS\n\nDate/Time: $LONGDATETIME\n\"";
 $output = shell_exec($command);
 echo $output;
+shell_exec('chown nagios:nagios /var/log/sendemail'); // Log gets created as running user. Fix.
 ?>
