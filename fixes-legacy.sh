cc354c024353044626cd1dfe4a84f4ae4619a04f
Robbie Ferguson <contact@category5.tv>
2018-05-21 20:55:18 -0400

    Update fixes-legacy.sh

--- fixes-legacy.sh
+++ fixes-legacy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Running legacy fixes... you may want to consider upgrading your NEMS version."
 
+ # using hard file location rather than symlink as symlink may not exist yet on older versions
+ platform=$(/usr/local/share/nems/nems-scripts/info.sh platform)
+ ver=$(/usr/local/share/nems/nems-scripts/info.sh nemsver) 
+
 # NEMS 1.2.1 was released with an incorrect permission on this file
 if [[ $ver = "1.2.1" ]]; then
   chown www-data:www-data /etc/nagios3/global/timeperiods.cfg
