fe47586e6d606a8c0356a57d39b9a35d7eacae8b
Robbie Ferguson <contact@category5.tv>
2018-05-21 20:52:59 -0400

    Move first item to legacy fixes, test to ensure $ver is passed.

--- fixes-legacy.sh
+++ fixes-legacy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Running legacy fixes... you may want to consider upgrading your NEMS version."
+
+# NEMS 1.2.1 was released with an incorrect permission on this file
+if [[ $ver = "1.2.1" ]]; then
+  chown www-data:www-data /etc/nagios3/global/timeperiods.cfg
+fi
+
+echo $ver
+
 exit
