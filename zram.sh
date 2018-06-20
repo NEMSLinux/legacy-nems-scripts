12f876cd64e4ed2f8a6ad07485b8a350588793f8
NEMS Linux <nems@baldnerd.com>
2018-06-21 06:03:47 +1245

    Don't affect swap if ZRAM isn't possible

--- zram.sh
+++ zram.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 cores=$(nproc --all)
-modprobe zram num_devices=$cores
+modprobe zram num_devices=$cores > /dev/null 2>&1
 
-swapoff -a
+result=$(cat /proc/modules | grep zram)
 
-totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
-mem=$(( ($totalmem / $cores)* 1024 ))
+if [[ $result = *"zram"* ]]; then
 
-core=0
-while [ $core -lt $cores ]; do
-  echo $mem > /sys/block/zram$core/disksize
-  mkswap /dev/zram$core
-  swapon -p 5 /dev/zram$core
-  let core=core+1
-done
+  swapoff -a
 
+  totalmem=`free | grep -e "^Mem:" | awk '{print $2}'`
+  mem=$(( ($totalmem / $cores)* 1024 ))
+
+  core=0
+  while [ $core -lt $cores ]; do
+    echo $mem > /sys/block/zram$core/disksize
+    mkswap /dev/zram$core
+    swapon -p 5 /dev/zram$core
+    let core=core+1
+  done
+else
+  echo ZRAM Not Found...
+fi
