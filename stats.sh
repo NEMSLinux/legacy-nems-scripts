79caf24325b966e57cae1f77bfd9186c25733cb0
NEMS Linux <nems@baldnerd.com>
2018-07-17 23:56:48 -0400

    Create log for NEMS Stats

--- stats.sh
+++ stats.sh
@@ -10,6 +10,13 @@ if (!file_exists('/var/log/nems/hw_model')) {
   sleep(30);
 }
 
+
+$output = date('r') . PHP_EOL;
+$load = sys_getloadavg();
+$output .= 'LA: ' . $load[0] . PHP_EOL;
+$output .= 'Sending anonymous stats to https://nemslinux.com/stats/' . PHP_EOL;
+file_put_contents('/var/log/nems/stats.log',$output,FILE_APPEND);
+
 if (file_exists('/var/log/nems/hw_model')) { // Don't run this until system is ready to report true stats
 
   // Get the platform of your NEMS server
@@ -70,6 +77,8 @@ if (file_exists('/var/log/nems/hw_model')) { // Don't run this until system is r
     'timezone'=>date('T'),
   );
 
+  file_put_contents('/var/log/nems/stats.log',serialize($data) . PHP_EOL,FILE_APPEND);
+
   // Load existing NEMS Stats API Key, if it exists
   $settings = @file('/usr/local/share/nems/nems.conf');
   if (is_array($settings) && count($settings) > 0) {
@@ -83,7 +92,6 @@ if (file_exists('/var/log/nems/hw_model')) { // Don't run this until system is r
   $ch = curl_init('https://nemslinux.com/api/stats/');
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-//  $response = curl_exec($ch);
   $retry = 0;
   $newkey = '';
   while((curl_errno($ch) == 28 || $newkey == '') && $retry < 1440){ // error 28 is timeout - retry every 5 seconds for 1440 tries (2 hours). Will also retry if an apikey is not sent by the server.
@@ -91,14 +99,25 @@ if (file_exists('/var/log/nems/hw_model')) { // Don't run this until system is r
     sleep(5);
     $newkey = filter_var($response,FILTER_SANITIZE_STRING);
     if (strlen($newkey) > 0) break;
+    file_put_contents('/var/log/nems/stats.log','Failed to send stats. Trying again.' . PHP_EOL,FILE_APPEND);
     $retry++;
   }
-
+  if ($retry < 1440) {
+    file_put_contents('/var/log/nems/stats.log','Success.' . PHP_EOL,FILE_APPEND);
+  } else {
+    file_put_contents('/var/log/nems/stats.log','Failed. Giving up.' . PHP_EOL,FILE_APPEND);
+  }
   curl_close($ch);
   if (!isset($data['apikey'])) {
     $data['apikey'] = $newkey; // no API Key in settings, use the new one
     file_put_contents('/usr/local/share/nems/nems.conf','apikey=' . $newkey . PHP_EOL, FILE_APPEND);
+    file_put_contents('/var/log/nems/stats.log','Assigned new API Key by server: ' . $newkey . PHP_EOL,FILE_APPEND);
   }
 
 }
+
+$load = sys_getloadavg();
+$output = 'LA: ' . $load[0] . PHP_EOL;
+file_put_contents('/var/log/nems/stats.log',$output . '--------------------' . PHP_EOL,FILE_APPEND);
+
 ?>
