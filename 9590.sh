f00c87d53ac3b4b399f467bd356b24082aafe4cc
NEMS Linux <nems@baldnerd.com>
2018-07-22 21:38:55 -0400

    Minor improvements to 9590

--- 9590.sh
+++ 9590.sh
@@ -4,20 +4,28 @@
 
 set -e
 
+pid=""
+if [[ -f /run/9590.pid ]]; then
+  pid=$(cat /run/9590.pid)
+fi
+
 case "${1:-}" in
   stop|reload|restart|force-reload)
         echo "Stopping 9590."
-        kill $(cat /run/9590.pid)
+        kill $pid
 	rm /run/9590.pid ;;
 
   start)
      if [[ -f /run/9590.pid ]]; then
-       echo "Already running."
-       exit 0
+       if ps -p $pid > /dev/null; then
+         echo "Already running."
+         exit 0
+       else
+         rm /run/9590.pid
+       fi
      fi
         echo "Starting 9590."
-        /bin/nc -l -p 9590 &
-        echo $! > /run/9590.pid ;;
+        /bin/nc -l -p 9590 & echo $! > /run/9590.pid ;;
 
   *)
         echo "Usage: ${0:-} {start|stop|status|restart|reload|force-reload}" >&2
