0b5d03d9d9cc8e007d4c5758922fc91d774b7b22
Robbie Ferguson <contact@category5.tv>
2018-07-25 09:38:57 -0400

    Add command line option to pass old socket path

--- stats/hosts.py
+++ stats/hosts.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python
-#
-# Sample program for accessing the Livestatus Module
-# from a python program
-socket_path = "/usr/local/nagios/var/rw/live.sock"
+import sys, getopt
+
+def main(argv):
+   socket_path = "/usr/local/nagios/var/rw/live.sock"
+   try:
+      opts, args = getopt.getopt(argv,"hs:",["socket="])
+   except getopt.GetoptError:
+      print './hosts.py -s <socket_path>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt in ("-h", "--help"):
+         print './hosts.py -s <socket_path>'
+         sys.exit()
+      elif opt in ("-s", "--socket"):
+         socket_path = arg
+          
+print 'Using socket "', socket_path
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+    
 
 import socket
 s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -17,8 +34,8 @@ s.shutdown(socket.SHUT_WR)
 
 # Now read the answer
 answer = s.recv(100000000)
+print answer
 
 # Parse the answer into a table (a list of lists)
-table = [ line.split(';') for line in answer.split('\n')[:-1] ]
-
-print table
+#table = [ line.split(';') for line in answer.split('\n')[:-1] ]
+#print table
