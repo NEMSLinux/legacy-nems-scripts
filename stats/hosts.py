da1e89372e75b0549e28aa778a19f290208c7ef7
Robbie Ferguson <contact@category5.tv>
2018-07-25 09:05:14 -0400

    Setup initial socket query for hosts

--- stats/hosts.py
+++ stats/hosts.py
@@ -1,1 +1,24 @@
-Dummy file
+#!/usr/bin/python
+#
+# Sample program for accessing the Livestatus Module
+# from a python program
+socket_path = "/usr/local/nagios/var/rw/live.sock"
+
+import socket
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+s.connect(socket_path)
+
+# Write command to socket
+s.send("GET hosts\n")
+
+# Important: Close sending direction. That way
+# the other side knows we are finished.
+s.shutdown(socket.SHUT_WR)
+
+# Now read the answer
+answer = s.recv(100000000)
+
+# Parse the answer into a table (a list of lists)
+table = [ line.split(';') for line in answer.split('\n')[:-1] ]
+
+print table
