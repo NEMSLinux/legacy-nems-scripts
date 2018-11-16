d68112ad87dd98fe0848bf2e191aeb9e0e4cbec7
NEMS Linux <nems@baldnerd.com>
2018-11-16 09:39:21 -0500

    Auto reinstall corrupt repos.

--- update.sh
+++ update.sh
@@ -18,43 +18,138 @@ else
   
   # Update nems-migrator
   printf "Updating nems-migrator... "
-  cd /root/nems/nems-migrator && git pull
-  echo "Done."
-
+  cd /root/nems/nems-migrator
+  result=`git pull`
+  echo $result
+  if [[ $result =~ 'error:' ]]; then
+    echo 'Error detected. Reinstalling...';
+    mv /root/nems/nems-migrator /root/nems/nems-migrator~
+    cd /root/nems
+    git clone https://github.com/Cat5TV/nems-migrator
+    if [[ -d /root/nems/nems-migrator ]]; then
+      echo 'Successfully reinstalled.'
+      rm -rf /root/nems/nems-migrator~
+    else
+      echo 'Reinstall failed. Please copy your backup.nems file and re-image your device.'
+      mv /root/nems/nems-migrator~ /root/nems/nems-migrator
+    fi
+  fi
   # Get the latest version data from nems-migrator
   cp -f /root/nems/nems-migrator/data/nems/ver-current.txt /var/www/html/inc/ver-available.txt
+  echo "Done."
 
   # Update nems-www
   printf "Updating nems-www... "
-  cd /var/www/html && git pull
+  cd /var/www/html
+  result=`git pull`
+  echo $result
+  if [[ $result =~ 'error:' ]]; then
+    echo 'Error detected. Reinstalling...';
+    mv /var/www/html /var/www/nems-www~
+    cd /var/www
+    git clone https://github.com/Cat5TV/nems-www
+    if [[ -d /var/www/nems-www ]]; then
+      mv nems-www html
+      chown -R www-data:www-data html
+      echo 'Successfully reinstalled.'
+      rm -rf /var/www/nems-www~
+    else
+      echo 'Reinstall failed. Please copy your backup.nems file and re-image your device.'
+      mv /var/www/nems-www~ /var/www/html
+    fi
+  fi
   echo "Done."
 
   # Update nems-admin
   printf "Updating nems-admin... "
-  cd /root/nems/nems-admin && git pull
+  cd /root/nems/nems-admin
+  result=`git pull`
+  echo $result
+  if [[ $result =~ 'error:' ]]; then
+    echo 'Error detected. Reinstalling...';
+    mv /root/nems/nems-admin /root/nems/nems-admin~
+    cd /root/nems
+    git clone https://github.com/Cat5TV/nems-admin
+    if [[ -d /root/nems/nems-admin ]]; then
+      echo 'Successfully reinstalled.'
+      rm -rf /root/nems/nems-admin~
+    else
+      echo 'Reinstall failed. Please copy your backup.nems file and re-image your device.'
+      mv /root/nems/nems-admin~ /root/nems/nems-admin
+    fi
+  fi
   echo "Done."
 
   # Update nems-nconf
-  printf "Updating nems-conf... "
-  cd /var/www/nconf && git pull
+  printf "Updating nconf... "
+  cd /var/www/nconf
+  result=`git pull`
+  echo $result
+  if [[ $result =~ 'error:' ]]; then
+    echo 'Error detected. Reinstalling...';
+    mv /var/www/nconf /var/www/nconf~
+    cd /var/www
+    git clone https://github.com/Cat5TV/nconf
+    if [[ -d /var/www/nconf ]]; then
+      echo 'Successfully reinstalled.'
+      rm -rf /var/www/nconf~
+    else
+      echo 'Reinstall failed. Please copy your backup.nems file and re-image your device.'
+      mv /var/www/nconf~ /var/www/nconf
+    fi
+  fi
   echo "Done."
 
   # Update nems-tools
   printf "Updating nems-tools... "
   if [[ -d /root/nems/nems-tools ]]; then
-    cd /root/nems/nems-tools && git pull
+    cd /root/nems/nems-tools
+    result=`git pull`
+    echo $result
+    if [[ $result =~ 'error:' ]]; then
+      echo 'Error detected. Reinstalling...';
+      mv /root/nems/nems-tools /root/nems/nems-tools~
+      cd /root/nems
+      git clone https://github.com/Cat5TV/nems-tools
+      if [[ -d /root/nems/nems-tools ]]; then
+        echo 'Successfully reinstalled.'
+        rm -rf /root/nems/nems-tools~
+      else
+        echo 'Reinstall failed. Please copy your backup.nems file and re-image your device.'
+        mv /root/nems/nems-tools~ /root/nems/nems-tools
+      fi
+    fi
     echo "Done."
   else
     echo "nems-tools is not installed."
+    cd /root/nems
+    git clone https://github.com/Cat5TV/nems-tools
+    echo 'Installed nems-tools.'
   fi
 
   # Update self
   printf "Updating nems-scripts... "
   if [ -d /usr/local/share/nems/nems-scripts ]; then
-    cd /usr/local/share/nems/nems-scripts && git pull
+    cd /usr/local/share/nems/nems-scripts
+    result=`git pull`
+    echo $result
+    if [[ $result =~ 'error:' ]]; then
+      echo 'Error detected. Reinstalling...';
+      mv /usr/local/share/nems/nems-scripts /usr/local/share/nems/nems-scripts~
+      cd /usr/local/share/nems
+      git clone https://github.com/Cat5TV/nems-scripts
+      if [[ -d /usr/local/share/nems ]]; then
+        echo 'Successfully reinstalled.'
+        rm -rf /usr/local/share/nems/nems-scripts~
+      else
+        echo 'Reinstall failed. Please copy your backup.nems file and re-image your device.'
+        mv /usr/local/share/nems/nems-scripts~ /usr/local/share/nems/nems-scripts
+      fi
+    fi
   fi
   # Check if we are still on legacy 1.1 or 1.2.x and update that way, otherwise fixes will not run to patch it
   if [ -d /home/pi/nems-scripts ]; then
+    echo "*** You need to upgrade your NEMS server to a current version! ***"
     cd /home/pi/nems-scripts && git pull
   fi
   echo "Done."
