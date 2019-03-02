c4fd1ba54c3cfd743040afbd9a95f7db556890c7
NEMS Linux <nems@baldnerd.com>
2019-03-02 11:39:16 -0500

    Improve connected display

--- bootscreen.sh
+++ bootscreen.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-ver=$(/usr/local/bin/nems-info nemsver)
-dialog --title "NEMS Linux $ver" --infobox "\
-Refreshing...
-" 20 72
-veravail=$(/usr/local/bin/nems-info nemsveravail)
 
+# Global variables
+ver=$(/usr/local/bin/nems-info nemsver)
 host=$(/bin/hostname)
+
+display_screen() {
+  if [[ $2 == 'warning' ]]; then
+    conf=/usr/local/share/nems/nems-scripts/settings/dialog.warning
+  else
+    conf=/usr/local/share/nems/nems-scripts/settings/dialog.normal
+  fi
+  env DIALOGRC=$conf dialog --title "$1" \
+    --no-collapse \
+    --infobox "$output" 20 72
+}
+
+# Loading screen
+output="Loading..."
+display_screen "NEMS Linux $ver"
+
+
+while :
+do
+
+alias=$(/usr/local/bin/nems-info alias)
+veravail=$(/usr/local/bin/nems-info nemsveravail)
 users=$(/usr/local/bin/nems-info users)
 cpupercent=$(/usr/local/bin/nems-info cpupercent)
 diskusage=$(/usr/local/bin/nems-info diskusage)
@@ -29,7 +48,14 @@ if [[ $init == "0" ]]; then
   output_init="Your NEMS server is not yet initialized. Please run: sudo nems-init"
 fi
 
-dialog --title "NEMS Linux $ver" --infobox "\
+if (( $ver < $veravail )); then
+  current="- $veravail is Available."
+else
+  current=""
+fi
+output="\
+NEMS Version:     $ver $current\n\
+Server Alias:     $alias\n\
 Platform:         $platform_name\n\
 Hostname:         $host.local\n\
 IP Address:       $ip\n\
@@ -41,5 +67,23 @@ Internet Status:  $internet\n\
 \n\
 To login, use SSH or press CTRL-ALT-F2\n\
 \n\
-For help, visit: docs.nemslinux.com" 20 72 # make these number match the refreshing note
-sleep 30
+For help, visit: docs.nemslinux.com"
+display_screen "NEMS Server Overview"
+
+sleep 10
+
+# NEMS Warning Light Current State Screen
+  if [[ -e /var/log/nems/nems-tools/currentstate ]]; then
+    output=$(cat /var/log/nems/nems-tools/currentstate)
+    if [[ $output == *"CRITICAL"* ]]; then
+      conf="warning"
+      timer=45
+    else
+      conf="normal"
+      timer=10
+    fi
+    display_screen "NEMS Current State" $conf
+    sleep $timer
+  fi
+
+done
