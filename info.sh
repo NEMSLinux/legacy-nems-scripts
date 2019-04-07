97223a69f0c52946bb97a0234d899cd5a47c1b36
NEMS Linux <nems@baldnerd.com>
2019-04-07 00:31:51 -0400

    Fix HWID on Virtual Appliance where NIC isn't same

--- info.sh
+++ info.sh
@@ -119,7 +119,7 @@ elif [[ $COMMAND == "hwid" ]]; then
     /sbin/ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | md5sum | cut -d"-" -f1 -
   # Virtual Appliance
   elif (( $platform == 20 )); then 
-    /sbin/ifconfig enp0s3 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | md5sum | cut -d"-" -f1 -
+    /sbin/ifconfig $(/usr/local/bin/nems-info nic) | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | md5sum | cut -d"-" -f1 -
   # ODROID-N2
   elif (( $platform >= 15 )) && (( $platform <= 16 )); then
     cat /proc/cpuinfo | grep Serial |  printf '%s' $(cut -n -d ' ' -f 2) | md5sum | cut -d"-" -f1 -
