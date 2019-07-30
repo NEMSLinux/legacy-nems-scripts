896058f70f5e1ca0d58a141611e1def030e2c6d1
NEMS Linux <nems@baldnerd.com>
2019-07-29 22:11:40 -0400

    Activate check_temper

--- fixes.sh
+++ fixes.sh
@@ -313,6 +313,13 @@ if (( $(awk 'BEGIN {print ("'$ver'" >= "'1.5'")}') )); then
    /root/nems/nems-admin/build/500-temper
  fi
 
+ # Install check_temper script
+ if [[ ! -e /usr/lib/nagios/plugins/check_temper ]]; then
+   cp /root/nems/nems-migrator/data/1.5/nagios/plugins/check_temper /usr/lib/nagios/plugins/check_temper
+   printf -- "\e[37mImporting:\e[97m check_temper to NEMS NConf\033[0m\n" && /var/www/nconf/bin/add_items_from_nagios.pl -c checkcommand -f /root/nems/nems-migrator/data/1.5/nagios/nconf_patches/check_temper.cfg 2>&1 | grep -E "ERROR"
+   echo 'KERNEL=="hidraw1", MODE="0666"' > /etc/udev/rules.d/temper.rules
+ fi
+
  # Install nems-webhooktest command if not already
  if [ ! -f /usr/local/bin/nems-webhooktest ]; then
    ln -s /usr/local/share/nems/nems-scripts/webhooktest.sh /usr/local/bin/nems-webhooktest
