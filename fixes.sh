72c3cc6a0c64ff0f2f2718e43bd7fabfc4d4c2cc
NEMS Linux <nems@baldnerd.com>
2019-09-06 10:42:25 -0400

    Markdown in Telegram

--- fixes.sh
+++ fixes.sh
@@ -335,7 +335,7 @@ if (( $(awk 'BEGIN {print ("'$ver'" >= "'1.5'")}') )); then
  fi
 
  # Upgrade Telegram
- telegramver=1.5.4 # Current version of Telegram script
+ telegramver=1.5.6 # Current version of Telegram script
  if ! grep -q "VERSION $telegramver" /usr/lib/nagios/plugins/notify-by-telegram.lua; then
    cp -f /root/nems/nems-migrator/data/1.5/nagios/plugins/notify-by-telegram.lua /usr/lib/nagios/plugins/notify-by-telegram.lua
  fi
