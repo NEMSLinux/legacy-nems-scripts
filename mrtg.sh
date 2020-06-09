806f619d0786c77b72b527cfb1122e51209db722
NEMS Linux <nems@baldnerd.com>
2020-06-09 09:43:40 -0400

    Add error handling if no SNMP

--- mrtg.sh
+++ mrtg.sh
@@ -28,6 +28,16 @@ fi
 /usr/local/mrtg2/bin/cfgmaker --global 'WorkDir: /var/www/mrtg' --global 'Options[_]: bits,growright' --output /etc/mrtg/mrtg.cfg public@${routerip}
 env LANG=C /usr/local/mrtg2/bin/mrtg /etc/mrtg/mrtg.cfg
 echo ""
-echo "Here are the generated links, which will automatically be updated every 5 minutes:"
-find /var/www/mrtg/*.html  -printf " - https://nems.local/mrtg/%f\n"
+count=$(ls -1 /var/www/mrtg/*.html 2>/dev/null | wc -l)
+if [ $count != 0 ]; then
+  echo "Here are the generated links, which will automatically be updated every 5 minutes:"
+  find /var/www/mrtg/*.html  -printf " - https://nems.local/mrtg/%f\n"
+else
+  echo "*******************************************************************"
+  echo -e "\e[1mNo data found.\e[0m"
+  echo "Are you certain your router supports SNMP and you enabled it first?"
+  echo "Are you sure you entered the correct IP address for the router?"
+  echo "Nothing to detect."
+  echo "*******************************************************************"
+fi
 echo ""
