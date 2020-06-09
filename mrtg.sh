b4741e5f095d4aef80b7c172d553482ce4cbd0e7
NEMS Linux <nems@baldnerd.com>
2020-06-09 07:48:06 -0400

    Auto-detect gateway for mrtg

--- mrtg.sh
+++ mrtg.sh
@@ -6,7 +6,25 @@ echo "See https://docs.nemslinux.com/usage/mrtg"
 echo ""
 echo "MAKE SURE SNMP IS ALREADY ENABLED ON YOUR ROUTER"
 echo ""
-read -p "What is your router's IP address? (CTRL-C to abort) " routerip
+gateway=$(ip r | grep default | awk '{print $3}' | head -n 1)
+routerip=""
+if [[ ! $gateway == "" ]]; then
+ read -r -p "I detected $gateway. Do you want to use that? [y/N] " detected
+    echo ""
+    if [[ $detected =~ ^([yY][eE][sS]|[yY])$ ]]; then
+      echo "Using $gateway."
+      routerip=$gateway
+    fi
+fi
+if [[ $routerip == "" ]]; then
+  read -p "What is your router's IP address? (CTRL-C to abort) " routerip
+fi
+if [[ $routerip == "" ]]; then
+  echo
+  echo "No IP provided for the router. Aborting."
+  echo
+  exit 1
+fi
 /usr/local/mrtg2/bin/cfgmaker --global 'WorkDir: /var/www/mrtg' --global 'Options[_]: bits,growright' --output /etc/mrtg/mrtg.cfg public@${routerip}
 env LANG=C /usr/local/mrtg2/bin/mrtg /etc/mrtg/mrtg.cfg
 echo ""
